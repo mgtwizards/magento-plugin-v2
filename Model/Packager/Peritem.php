7616d657e1f7de45975ba9ed82693ee2b74788b4
andyvance <andy@porterbuddy.com>
2020-11-06 12:02:08 +0100

    Updates to fix dimension issue

--- Model/Packager/Peritem.php
+++ Model/Packager/Peritem.php
@@ -4,26 +4,21 @@
  * @copyright Copyright (c) 2018 Convert (http://www.convert.no/)
  */
 namespace Porterbuddy\Porterbuddy\Model\Packager;
-
 use Magento\Catalog\Model\Product\Type;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\DataObject;
 use Porterbuddy\Porterbuddy\Model\Carrier;
-
 class Peritem implements PackagerInterface
 {
     const MODE = 'per_item';
-
     /**
      * @var \Porterbuddy\Porterbuddy\Helper\Data
      */
     protected $helper;
-
     /**
      * @var \Magento\Framework\Event\ManagerInterface
      */
     protected $eventManager;
-
     public function __construct(
         \Porterbuddy\Porterbuddy\Helper\Data $helper,
         \Magento\Framework\Event\ManagerInterface $eventManager
@@ -31,7 +26,6 @@ class Peritem implements PackagerInterface
         $this->eventManager = $eventManager;
         $this->helper = $helper;
     }
-
     public function estimateParcels(\Magento\Quote\Model\Quote\Address\RateRequest $request)
     {
         $parcels = [];
@@ -42,12 +36,10 @@ class Peritem implements PackagerInterface
             }
             $product = $item->getProduct();
             $dimensions = $this->getDimensions($product);
-
             $qty = $item->getQty();
             if ($item->getParentItem() && $item->getParentItem()->getQty() > 0) {
                 $qty *= $item->getParentItem()->getQty();
             }
-
             $weight = $this->helper->convertWeightToGrams(
                 $item->getWeight() ?: $this->helper->getDefaultProductWeight()
             );
@@ -61,17 +53,14 @@ class Peritem implements PackagerInterface
                 ];
             }
         }
-
         $transport = new DataObject(['parcels' => $parcels]);
         $this->eventManager->dispatch('porterbuddy_estimate_parcels_per_item', [
             'transport' => $transport,
             'request' => $request,
         ]);
         $parcels = $transport->getData('parcels');
-
         return $parcels;
     }
-
     /**
      * @param \Magento\Quote\Model\Quote\Item $item
      * @return bool
@@ -82,42 +71,34 @@ class Peritem implements PackagerInterface
     protected function canShip(\Magento\Quote\Model\Quote\Item $item)
     {
         $result = true;
-
         if ($item->getIsVirtual()) {
             $result = false;
         }
-
         $hasChildren = count($item->getChildren()) > 0;
         $isShipSeparately = $item->isShipSeparately();
         $hasParent = (bool)$item->getParentItem();
-
         // TODO: more beautiful and extensible
         if (Type::TYPE_BUNDLE == $item->getProductType()) {
             // always use children
             $result = false;
         }
-
         if ($hasChildren && $isShipSeparately) {
             // ship separately - skip parent, ship children
             $result = false;
         }
-
         // TODO: more beautiful and extensible
         if ($hasParent && !$isShipSeparately && Type::TYPE_BUNDLE !== $item->getParentItem()->getProductType()) {
             // Magento default - ship parent, skip children
             $result = false;
         }
-
         $transport = new DataObject(['result' => $result]);
         $this->eventManager->dispatch('porterbuddy_estimate_parcels_per_item_can_ship', [
             'transport' => $transport,
             'quote_item' => $item,
         ]);
         $result = $transport->getData('result');
-
         return $result;
     }
-
     /**
      * Creates single big package out of all items
      *
@@ -129,7 +110,6 @@ class Peritem implements PackagerInterface
         /** @var \Magento\Sales\Model\Order\Shipment\Item $item */
         foreach ($request->getOrderShipment()->getAllItems() as $item) {
             $product = $item->getOrderItem()->getProduct();
-
             $parent = $item->getOrderItem()->getParentItem();
             if ($parent && Configurable::TYPE_CODE == $parent->getProductType()) {
                 // skip child of configurable, all info in parent
@@ -139,9 +119,7 @@ class Peritem implements PackagerInterface
                 // skip parent bundle item, bundle children items are following
                 continue;
             }
-
             $dimensions = $this->getDimensions($product);
-
             $weight = $this->helper->convertWeightToGrams(
                 $item->getWeight() ?: $this->helper->getDefaultProductWeight()
             );
@@ -172,23 +150,19 @@ class Peritem implements PackagerInterface
                     ]
                 ],
             ];
-
             $qty = $item->getQty();
             for ($i = 0; $i < $qty; $i++) {
                 $packages[] = $package;
             }
         }
-
         $transport = new DataObject(['packages' => $packages]);
         $this->eventManager->dispatch('porterbuddy_prepare_create_packages_per_item', [
             'transport' => $transport,
             'request' => $request,
         ]);
         $packages = $transport->getData('packages');
-
         return $packages;
     }
-
     /**
      * @param \Magento\Catalog\Model\Product $product
      * @param bool $useDefaults
@@ -196,23 +170,18 @@ class Peritem implements PackagerInterface
      */
     public function getDimensions(\Magento\Catalog\Model\Product $product)
     {
-
         $returnDimensions = [
             'width' => $this->helper->getDefaultProductWidth(),
             'length' => $this->helper->getDefaultProductLength(),
             'height' => $this->helper->getDefaultProductHeight(),
         ];
-
         $definedDimensions = [
             'width' => $this->getAttributeValue($product, $this->helper->getWidthAttribute()),
             'length' => $this->getAttributeValue($product, $this->helper->getLengthAttribute()),
             'height' => $this->getAttributeValue($product, $this->helper->getHeightAttribute()),
         ];
-
         $definedDimensions = array_filter($definedDimensions);
-
-        $returnDimensions += $definedDimensions;
-
+        $returnDimensions = array_merge($returnDimensions, $definedDimensions);
         $returnDimensions = [
             'width' => $this->helper->convertDimensionToCm($returnDimensions['width']),
             'length' => $this->helper->convertDimensionToCm($returnDimensions['length']),
@@ -220,7 +189,6 @@ class Peritem implements PackagerInterface
         ];
         return $returnDimensions;
     }
-
     /**
      * @param \Magento\Catalog\Model\Product $product
      * @param string $attribute
